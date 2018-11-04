7ac0bebaf4b0c5701b8f80aaaec9a599b707336a
KODING WARRIOR <rijgndqw012@gmail.com>
2018-11-04 20:12:09 +0900

    minor change for settings.py

--- todoquest/settings.py
+++ todoquest/settings.py
@@ -90,6 +90,13 @@ WSGI_APPLICATION = 'todoquest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
