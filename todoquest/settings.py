29c46300c68cf779a7d568e6cdbe3ef9432a4efe
KODING WARRIOR <rijgndqw012@gmail.com>
2018-11-04 19:14:26 +0900

    MODIFY STATIC FILES PATH

--- todoquest/settings.py
+++ todoquest/settings.py
@@ -133,4 +133,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = [
+        os.path.join(PROJECT_ROOT, 'static'),
+    ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
