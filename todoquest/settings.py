6e17b2a05c5a03e6792355805a0cec09e016bf66
KODING WARRIOR <rijgndqw012@gmail.com>
2018-11-04 19:46:03 +0900

    ADD DJANGO-HEROKU DEPENDENCY

--- todoquest/settings.py
+++ todoquest/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import djnago_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -143,3 +145,5 @@ STATICFILES_DIR = [
     ]
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
