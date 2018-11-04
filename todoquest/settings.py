57939d11e5b54a50bf6aa87a67efcc71e8794e07
KODING WARRIOR <rijgndqw012@gmail.com>
2018-11-04 20:59:04 +0900

    Changing to Mongo DB

--- todoquest/settings.py
+++ todoquest/settings.py
@@ -92,13 +92,14 @@ WSGI_APPLICATION = 'todoquest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': os.environ['NAME'],
+        'HOST': os.environ['HOST'],
+        'USER': os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
+        'PORT': os.environ['PORT']
     }
 }
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
